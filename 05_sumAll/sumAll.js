298d826c997c5027c925159f507adf0a67e50204
Shovelfather <shovelfather@gmail.com>
2024-04-19 17:40:09 -0400

    ex05 - solved
    Learned a bit about isNaN() and Number.isInteger to handle errors here.
    Basic but this is the kind of real-world stuff that'll help a bunch in
    the future.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x <= 0 || y <= 0 || !Number.isInteger(x) || !Number.isInteger(y))
+    return "ERROR";
+  let lower = Math.min(x, y);
+  let upper = Math.max(x, y);
+  let sum = 0;
 
+  for (i = lower; i < upper + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
