425b784540c8f4b282ec251680f24173c7d53b0a
Shovelfather <shovelfather@gmail.com>
2024-04-19 18:04:44 -0400

    ex04 - solved
    Look into the rest operator (...)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+  let filteredArray = [];
+  array.forEach(item => {
+    if (!itemsToRemove.includes(item)) { filteredArray.push(item); }
+  });
+  return filteredArray;
 };
 
 // Do not edit below this line
