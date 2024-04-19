138a0a5d93759c397c312e642793458240c6e821
Shovelfather <shovelfather@gmail.com>
2024-04-19 17:46:24 -0400

    ex02 - solved
    Had to add a little error handling here to get it working.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, timesToRepeat) {
+  if (timesToRepeat < 0) { return "ERROR"; }
+  let outputString = "";
+  for (i = 0; i < timesToRepeat; i++) {
+    outputString = outputString + stringToRepeat;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
